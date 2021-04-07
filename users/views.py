539e0ca5d59a43ceefddc7372bf56f2a51829825
n4u77i <nautaxhahmad125@gmail.com>
2021-04-07 21:53:27 +0500

    Initial Commit

---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created! You are now able to login')
+
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        uForm = UserUpdateForm(request.POST, instance=request.user)
+        pForm = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if uForm.is_valid() and pForm.is_valid():
+            uForm.save()
+            pForm.save()
+
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    else:
+        uForm = UserUpdateForm(instance=request.user)
+        pForm = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'uForm': uForm,
+        'pForm': pForm
+    }
+    return render(request, 'users/profile.html', context=context)
